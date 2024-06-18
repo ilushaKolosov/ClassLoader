43dedba071bb0ad78a055281368a7ed3ac1942f5
ilushaKolosov <126789172+ilushaKolosov@users.noreply.github.com>
2024-06-18 15:54:48 +0300

    .

---
+++ src/main/java/StartClass.java
@@ -0,0 +1,15 @@
+public class StartClass {
+    public static void main(String[] args) {
+        String classPath = "classes/Task3_ClassLoader";
+        CustomClassLoader customClassLoader = new CustomClassLoader(classPath);
+
+        try {
+            Class<?> helloWorldClass = customClassLoader.loadClass("example.HeyGuys");
+            Object helloWorldInstance = helloWorldClass.getDeclaredConstructor().newInstance();
+
+            helloWorldClass.getMethod("sayHello").invoke(helloWorldInstance);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
